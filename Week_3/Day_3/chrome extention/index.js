30915ffa6ad4c46ccb230806bcfdb89b653d037d
AhmedKhaled146 <a7med74yaso@gmail.com>
2025-10-28 15:32:58 +0300

    day 3 part one i built a chrome extention for save links

---
+++ Week_3/Day_3/chrome extention/index.js
@@ -0,0 +1,50 @@
+let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("save-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads);
+    });
+});
+
+function render(leads) {
+    ulEl.innerHTML = "";
+    for (let i = 0; i < leads.length; i++) {
+        ulEl.innerHTML += `
+            <li>
+                <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+    }
+}
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads);
+});
+
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    renderLeads(myLeads);
+});
